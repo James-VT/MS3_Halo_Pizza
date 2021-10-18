dde14f775563eabc18e74a8fab3e0861bececc0a
James-VT <jd_tanner@hotmail.co.uk>
2021-10-18 23:36:40 +0000

    add: Add register page and route

--- run.py
+++ run.py
@@ -4,10 +4,13 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-
+# The below line of code creates a new instance of Flask,
+# which is to be our app. So we store it in a variable
+# named app.
 app = Flask(__name__)
 
 # The below code is where we pull our environment variables from our
@@ -22,11 +25,19 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# The below route function gets us landing on our homepage by default
+# and allows that page to access the recipes in our database.
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("home.html", recipes=recipes)
+
+
+#The below route function  is for our registration page
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
