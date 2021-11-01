f52f140c69f404a54fb01167d20146e7500612cf
James-VT <jd_tanner@hotmail.co.uk>
2021-11-01 22:43:44 +0000

    add: Adds ability to dynamically add more ingredient fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
+
   });
+
+let next_ingredient_button = document.getElementById('next-ingredient-button');
+
+// The below code links to our add recipe page, and creates a new ingredient slot each time it is clicked
+next_ingredient_button.onclick = function(){
+    let nextIngredient = document.createElement('input');
+    nextIngredient.setAttribute('type', 'text');
+    nextIngredient.setAttribute('id', 'ingredients');
+    nextIngredient.setAttribute('name', 'ingredients');
+    nextIngredient.setAttribute('minlength', '4');
+    nextIngredient.setAttribute('required');
+}
