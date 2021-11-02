3026261d472913aa040530d9a0c3b3cb9a6d486c
James-VT <jd_tanner@hotmail.co.uk>
2021-11-02 22:26:32 +0000

    add: Adds HTML for a delete ingredient button

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
   });
 
 var next_ingredient_button = document.getElementById('next-ingredient-button');
-var ingredient_insert = document.getElementById('ingredient-insert')
+var ingredient_insert = document.getElementById('ingredient-insert');
+var delete_ingredient_button = document.getElementById('delete-ingredient-button');
 
 /** The below code links to our add recipe page, and creates a new ingredient slot each time it is clicked
  * Below code heavily borrowed from: https://www.youtube.com/watch?v=MLBLsxcB3Dc
@@ -17,5 +18,7 @@ next_ingredient_button.onclick = function(){
     nextIngredient.setAttribute('name', 'ingredients');
     nextIngredient.setAttribute('minlength', '4');
     nextIngredient.setAttribute('required', 'true');
+    nextIngredient.setAttribute('placeholder', 'Another ingredient');
     ingredient_insert.appendChild(nextIngredient);
 }
+
