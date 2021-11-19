4a855d77a130adf91b7d174623a4c95a5af8d274
James-VT <jd_tanner@hotmail.co.uk>
2021-11-19 12:15:58 +0000

    add: Adds search bar functionality and styling for it

--- app.py
+++ app.py
@@ -38,6 +38,13 @@ def get_recipes():
     return render_template("home.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("home.html", recipes=recipes)
+
+
 # The below route function is for our registration page
 @app.route("/register", methods=["GET", "POST"])
 def register():
